31af828268f83b75de87d4d8d11830dc45ae88fe
minecart <ben.frankson@icloud.com>
2024-02-20 21:45:15 -0500

    Add searchFunc and tester class findX

---
+++ searchFunc.java
@@ -0,0 +1,118 @@
+import java.util.Map;
+import java.util.ArrayList;
+
+public class searchFunc
+{
+    public static int sortedArraySearch(String[] array, String target, Map<String, String> map)
+    {
+        int current = (array.length - 1) / 2;
+        int modifier = current / 2;
+        ArrayList<Integer> tested = new ArrayList<Integer>();
+        
+        while (!target.equals(array[current]))
+        {
+            boolean overshot = Integer.parseInt(map.get(array[current])) > Integer.parseInt(map.get(target));
+            if (overshot)
+            {
+                current -= modifier;
+            }
+            else
+            {
+                current += modifier;
+            }
+                
+            /*
+            modifier /= 2;
+            if (modifier < 1)
+            {
+                modifier = 1;
+            }
+            */
+            
+            modifier = Math.max(modifier / 2, 1);
+            
+            
+            if (current < 0 || current >= array.length || tested.contains(current))
+            {
+                return -1;
+            }
+            
+            tested.add(current);
+        }
+        
+        return current;
+    }
+    
+    public static int sortedArraySearch(String[] array, String target)
+    {
+        int current = (array.length - 1) / 2;
+        int modifier = Math.max(current / 2, 1);
+        ArrayList<Integer> tested = new ArrayList<Integer>();
+        
+        while (!target.equals(array[current]))
+        {
+            boolean overshot = array[current].compareTo(target) > 0;
+            
+            if (overshot)
+            {
+                current -= modifier;
+            }
+            else
+            {
+                current += modifier;
+            }
+                
+            modifier = Math.max(modifier / 2, 1);
+            
+            if (current < 0 || current >= array.length || tested.contains(current))
+            {
+                return -1;
+            }
+            
+            tested.add(current);
+        }
+        
+        return current;
+    }
+    
+    public static int sortedArraySearch(int[] array, int target)
+    {
+        int current = (array.length - 1) / 2;
+        int modifier = current / 2;
+        ArrayList<Integer> tested = new ArrayList<Integer>();
+        
+        while (target != array[current])
+        {
+            boolean overshot = array[current] > target;
+            if (overshot)
+            {
+                current -= modifier;
+            }
+            else
+            {
+                current += modifier;
+            }
+            
+            // current = array[current] > target ? current - modifier : current + modifier;
+            
+            /*
+            modifier /= 2;
+            if (modifier < 1)
+            {
+                modifier = 1;
+            }
+            */
+            
+            modifier = Math.max(modifier / 2, 1);
+            
+            if (current < 0 || current >= array.length || tested.contains(current))
+            {
+                return -1;
+            }
+            
+            tested.add(current);
+        }
+        
+        return current;
+    }
+}
