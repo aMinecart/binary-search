bc42e10f7abc262122d021bcefdd1947345f5f96
minecart <ben.frankson@icloud.com>
2024-02-21 08:58:43 -0500

    Changed class to be static, reordered functions, and added comments

--- searchFunc.java
+++ searchFunc.java
@@ -1,27 +1,32 @@
 import java.util.Map;
 import java.util.ArrayList;
 
-public class searchFunc
+// creates a static class to hold the sortedArraySearch function, which finds the index of the
+// requested integer "target" in the given integer array "array" using a binary search algorithm
+//
+// sorterdArraySearch has two overloads - one for searching a String array sorted lexicographically,
+// and one for searching a String array sorted according to a map (see findX.java for an example)
+
+public static class searchFunc
 {
-    public static int sortedArraySearch(String[] array, String target, Map<String, Integer> map)
+    public static int sortedArraySearch(int[] array, int target)
     {
-        if (!map.containsKey(target))
-        {
-            return -1;
-        }
+        int current = (array.length - 1) / 2; // the index of the value in array currently being tested
+        int modifier = current / 2; // the number to add or subtract from current
+        ArrayList<Integer> tested = new ArrayList<Integer>(); // list containing all values previously tested
         
-        int current = (array.length - 1) / 2;
-        int modifier = current / 2;
-        ArrayList<Integer> tested = new ArrayList<Integer>();
-        
-        while (!target.equals(array[current]))
+        while (target != array[current])
         {
-            if (!map.containsKey(array[current]))
-            {
-                return -1;
-            }
-            
-            boolean overshot = map.get(array[current]) > map.get(target);
+            // current is in the middle of the unchecked section of the array
+            // 
+            // if the current's value is greater than target, than current is too high
+            // shift current to the middle of the section with lower values
+            // 
+            // otherwise, current is too low
+            // shift current to the middle of the section with higher values
+
+            /*
+            boolean overshot = array[current] > target;
             if (overshot)
             {
                 current -= modifier;
@@ -30,7 +35,15 @@ public class searchFunc
             {
                 current += modifier;
             }
-                
+            */
+            
+            // more concise
+            current = array[current] > target ? current - modifier : current + modifier;
+            
+            // modifier is halved to match current
+            // if modifier is equal to 0, current never changes and an infinite loop occurs
+            // keep modifier at a minimum of 1 to prevent this
+
             /*
             modifier /= 2;
             if (modifier < 1)
@@ -39,20 +52,25 @@ public class searchFunc
             }
             */
             
+            // more concise
             modifier = Math.max(modifier / 2, 1);
             
-            
+            // if out of bounds or current has already been tested, target is not in the array 
             if (current < 0 || current >= array.length || tested.contains(current))
             {
                 return -1;
             }
             
+            // add current to list of tested values
             tested.add(current);
         }
         
+        // array[current] == target
         return current;
     }
     
+    // the same as sortedArraySearch(int[] array, int target), but uses .compareTo()
+    // to determine the lexicographic position of strings
     public static int sortedArraySearch(String[] array, String target)
     {
         int current = (array.length - 1) / 2;
@@ -84,16 +102,30 @@ public class searchFunc
         
         return current;
     }
-    
-    public static int sortedArraySearch(int[] array, int target)
+
+    // the same as sortedArraySearch(int[] array, int target), but uses map.get()
+    // to order strings based off of their assigned integer 
+    public static int sortedArraySearch(String[] array, String target, Map<String, Integer> map)
     {
+        // if target has no value mapping in map, abort the search
+        if (!map.containsKey(target))
+        {
+            return -1;
+        }
+        
         int current = (array.length - 1) / 2;
         int modifier = current / 2;
         ArrayList<Integer> tested = new ArrayList<Integer>();
         
-        while (target != array[current])
+        while (!target.equals(array[current]))
         {
-            boolean overshot = array[current] > target;
+            // if current's value has no value mapping in map, abort the search
+            if (!map.containsKey(array[current]))
+            {
+                return -1;
+            }
+            
+            boolean overshot = map.get(array[current]) > map.get(target);
             if (overshot)
             {
                 current -= modifier;
@@ -103,17 +135,8 @@ public class searchFunc
                 current += modifier;
             }
             
-            // current = array[current] > target ? current - modifier : current + modifier;
-            
-            /*
-            modifier /= 2;
-            if (modifier < 1)
-            {
-                modifier = 1;
-            }
-            */
-            
             modifier = Math.max(modifier / 2, 1);
+            
             
             if (current < 0 || current >= array.length || tested.contains(current))
             {
