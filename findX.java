bc7d2f19c2c6c2aa2f4791c9cd281cde06d34e62
minecart <ben.frankson@icloud.com>
2024-02-20 22:00:41 -0500

    Updated searchFunc map generics
    Updated map generics in both searcFunc and findX to use Integer instead of String for the value type in key-value pairs. This avoids using Integer.parseInt in searchFunc to map values into integers, removing unnecessary overhead.

--- findX.java
+++ findX.java
@@ -3,51 +3,52 @@ import static java.util.Map.entry;
 
 public class findX
 {
-    private static Map<String, String> letToNumMap = Map.ofEntries
+    private static Map<String, Integer> letToNumMap = Map.ofEntries
         (
-            entry("a", "1"),
-            entry("b", "2"),
-            entry("c", "3"),
-            entry("d", "4"),
-            entry("e", "5"),
-            entry("f", "6"),
-            entry("g", "7"),
-            entry("h", "8"),
-            entry("i", "9"),
-            entry("j", "10"),
-            entry("k", "11"),
-            entry("l", "12"),
-            entry("m", "13"),
-            entry("n", "14"),
-            entry("o", "15"),
-            entry("p", "16"),
-            entry("q", "17"),
-            entry("r", "18"),
-            entry("s", "19"),
-            entry("t", "20"),
-            entry("u", "21"),
-            entry("v", "22"),
-            entry("w", "23"),
-            entry("x", "24"),
-            entry("y", "25"),
-            entry("z", "26")
+            entry("a", 1),
+            entry("b", 2),
+            entry("c", 3),
+            entry("d", 4),
+            entry("e", 5),
+            entry("f", 6),
+            entry("g", 7),
+            entry("h", 8),
+            entry("i", 9),
+            entry("j", 10),
+            entry("k", 11),
+            entry("l", 12),
+            entry("m", 13),
+            entry("n", 14),
+            entry("o", 15),
+            entry("p", 16),
+            entry("q", 17),
+            entry("r", 18),
+            entry("s", 19),
+            entry("t", 20),
+            entry("u", 21),
+            entry("v", 22),
+            entry("w", 23),
+            entry("x", 24),
+            entry("y", 25),
+            entry("z", 26)
         );
     
     public static void main(String[] args)
     {
         String find = "x";
-        String[] arr = {"a", "b", "c", "x", "y"};
+        String[] array = {"a", "b", "c", "x", "y"};
         
         String test = "catalytic";
-        String[] array = {"cat", "catastrophe", "cattle", "catz"};
+        String[] arr = {"cat", "catastrophe", "cattle", "catz"};
         
         int num = 1;
         int[] arguments = {1, 2, 3, 4, 7, 9};
         
-        int varPos = searchFunc.sortedArraySearch(arr, find, letToNumMap);
+        // throws an error the values of find and arr are not defined in letToNumMap
+        int varPos = searchFunc.sortedArraySearch(array, find, letToNumMap);
         System.out.println("The variable 'find' is position " + varPos + " of our array.");
         
-        int stringPos = searchFunc.sortedArraySearch(array, test);
+        int stringPos = searchFunc.sortedArraySearch(arr, test);
         System.out.println("The string 'test' is position " + stringPos + " of our array.");
         
         int numPos = searchFunc.sortedArraySearch(arguments, num);
